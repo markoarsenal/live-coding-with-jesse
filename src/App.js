943d1682a0ee714c9666224b6693a317afface2f
Marko Rajević <m.rajevic@levi9.com>
2017-08-30 10:26:42 +0200

    Header and sidebar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import { MenuItem } from 'material-ui/Menu';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import Drawer from 'material-ui/Drawer';
+import BottomNavigation, { BottomNavigationButton } from 'material-ui/BottomNavigation';
+import RestoreIcon from 'material-ui-icons/Restore';
+import FavoriteIcon from 'material-ui-icons/Favorite';
+import LocationOnIcon from 'material-ui-icons/LocationOn';
+
+injectTapEventPlugin();
 
 class App extends Component {
+	constructor (props) {
+		super(props);
+		this.state = { open: false }
+	}
+
+	toggleDrawer () {
+		this.setState(function (oldState) {
+			return { open: !oldState.open }
+		});
+	}
+	
 	render() {
 		return (
-			<div className="App">
-				<div className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h2>Welcome to React</h2>
-				</div>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-        		</p>
+			<div>
+				<AppBar position="static">
+					<Toolbar>
+						<IconButton color="contrast" aria-label="Menu" onClick={this.toggleDrawer.bind(this)}>
+							<MenuIcon />
+						</IconButton>
+						<Typography type="title" color="inherit">
+							Title
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<Drawer open={this.state.open} onRequestClose={this.toggleDrawer.bind(this)}>
+					<MenuItem>MenuItem</MenuItem>
+					<MenuItem>MenuItem 2</MenuItem>
+				</Drawer>
+				<BottomNavigation>
+					<BottomNavigationButton label="Recents" icon={<RestoreIcon />} />
+					<BottomNavigationButton label="Favorites" icon={<FavoriteIcon />} />
+					<BottomNavigationButton label="Nearby" icon={<LocationOnIcon />} />
+				</BottomNavigation>
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import createMuiTheme from 'material-ui/styles/theme';
+import { MuiThemeProvider } from 'material-ui/styles';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme();
+
+function ThemeApp () {
+    return (
+        <MuiThemeProvider theme={theme}>
+            <App />
+        </MuiThemeProvider>
+    );
+}
+
+ReactDOM.render(<ThemeApp />, document.getElementById('root'));
 registerServiceWorker();
